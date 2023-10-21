7997de635ff364fb3c81e5561d1334fbd120077e
Jan Cumps <jcumps@gmail.com>
2023-10-21 23:43:36 +0200

    reasonable successful move to lib

--- include/scpi/scpi_base.h
+++ include/scpi/scpi_base.h
@@ -3,6 +3,9 @@
 
 #include "scpi/scpi.h"
 
+#define SCPI_INPUT_BUFFER_LENGTH 256
+#define SCPI_ERROR_QUEUE_SIZE 17
+
 #define SCPI_BASE_COMMANDS \
     /* IEEE Mandated Commands (SCPI std V1999.0 4.1.1) */ \
     { .pattern = "*CLS", .callback = SCPI_CoreCls,}, \
@@ -48,5 +51,26 @@ scpi_result_t SCPI_VisaTrg(scpi_t * context);
 void doTrigger();
 
 scpi_bool_t scpi_instrument_input(const char * data, int len);
+
+scpi_t * getScpiContext();
+
+extern scpi_interface_t scpi_interface;
+extern char scpi_input_buffer[];
+extern scpi_error_t scpi_error_queue_data[];
+extern scpi_t scpi_context;
+
+void scpi_instrument_init();
+size_t SCPI_Write(scpi_t * context, const char * data, size_t len);
+int SCPI_Error(scpi_t * context, int_fast16_t err);
+scpi_result_t SCPI_Control(scpi_t * context, scpi_ctrl_name_t ctrl, scpi_reg_val_t val);
+scpi_result_t SCPI_Reset(scpi_t * context);
+scpi_result_t SCPI_Flush(scpi_t * context);
+
+// helper functions to simplyfy integration of TinyUSB tmcusb and the scpi-lib
+uint8_t getSTB();
+void setSTB(uint8_t);
+
+scpi_result_t SCPI_WriteReg(scpi_t * context, scpi_reg_name_t name);
+scpi_result_t SCPI_ReadReg(scpi_t * context, scpi_reg_name_t name);
 
 #endif // SCPI_SCPI_BASE_H
--- scpi/scpi_base.c
+++ scpi/scpi_base.c
@@ -1,5 +1,8 @@
 #include "scpi/scpi_base.h"
 
+#include "scpi-def.h"
+#include "usb/usbtmc_app.h"
+
 /**
  * Reimplement IEEE488.2 *TST?
  *
@@ -38,6 +41,98 @@ void doTrigger() {
     triggerHandler();
 }
 
+
+scpi_t scpi_context;
+
 scpi_bool_t scpi_instrument_input(const char * data, int len) {
     return SCPI_Input(&scpi_context, data, len);
+}
+
+scpi_interface_t scpi_interface = {
+    .error = NULL,            // haven't implemented an error logger
+    .write = SCPI_Write,
+    .control = SCPI_Control,
+    .flush = NULL,            // don't need flush for SCI / USB
+    .reset = SCPI_Reset,
+};
+
+char scpi_input_buffer[SCPI_INPUT_BUFFER_LENGTH];
+scpi_error_t scpi_error_queue_data[SCPI_ERROR_QUEUE_SIZE];
+
+
+// init helper for this instrument
+void scpi_instrument_init() {
+    initInstrument(); // if you prefer no dependency on the gpio_utils in main,
+              // you could move this call into the scpi_instrument_init() body.
+              // like I did here
+    
+     SCPI_Init(&scpi_context,
+             scpi_commands,
+             &scpi_interface,
+             scpi_units_def,
+             SCPI_IDN1, SCPI_IDN2, SCPI_IDN3, SCPI_IDN4,
+             scpi_input_buffer, SCPI_INPUT_BUFFER_LENGTH,
+             scpi_error_queue_data, SCPI_ERROR_QUEUE_SIZE);
+
+}
+
+
+
+/*
+ * The SCPI lib calls this function to write data back over the SCI2 interface
+ * visual clue: dim user LED B after delivering the reply
+ */
+size_t SCPI_Write(scpi_t * context, const char * data, size_t len) {
+    (void) context;
+
+  setReply(data, len);
+
+    return len;
+}
+
+scpi_result_t SCPI_Reset(scpi_t * context) {
+    (void) context;
+    initInstrument();
+    return SCPI_RES_OK;   
+}
+
+scpi_result_t SCPI_Control(scpi_t* context, scpi_ctrl_name_t ctrl, scpi_reg_val_t val)
+{
+    (void)context;
+    (void) val;
+
+    if (SCPI_CTRL_SRQ == ctrl) {
+        setControlReply();
+    }
+    return SCPI_RES_OK;
+}
+
+uint8_t getSTB() {
+    return (uint8_t) SCPI_RegGet(&scpi_context, SCPI_REG_STB);
+}
+
+void setSTB(uint8_t stb) {
+    SCPI_RegSet(&scpi_context, SCPI_REG_STB, (scpi_reg_val_t) stb);    
+}
+
+scpi_t * getScpiContext() {
+    return &scpi_context;
+}
+
+scpi_result_t SCPI_WriteReg(scpi_t * context, scpi_reg_name_t name) {
+    int32_t new_OPERE;
+    if (SCPI_ParamInt32(context, &new_OPERE, TRUE)) {
+        SCPI_RegSet(context, name, (scpi_reg_val_t) new_OPERE);
+    }
+    return SCPI_RES_OK;
+}
+
+scpi_result_t SCPI_ReadReg(scpi_t * context, scpi_reg_name_t name) {
+    /* return value */
+    SCPI_ResultInt32(context, SCPI_RegGet(context, name));
+
+    /* clear register */
+    SCPI_RegSet(context, name, 0);
+
+    return SCPI_RES_OK;
 }
