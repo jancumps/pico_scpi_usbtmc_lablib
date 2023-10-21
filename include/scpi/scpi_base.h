124102bf916a4700a49000ad901a5ab1e760f0a9
Jan Cumps <jcumps@gmail.com>
2023-10-21 22:36:16 +0200

    majority of base commands taken out of app

---
+++ include/scpi/scpi_base.h
@@ -0,0 +1,44 @@
+#ifndef SCPI_SCPI_BASE_H
+#define SCPI_SCPI_BASE_H
+
+#include "scpi/scpi.h"
+
+#define SCPI_BASE_COMMANDS \
+    /* IEEE Mandated Commands (SCPI std V1999.0 4.1.1) */ \
+    { .pattern = "*CLS", .callback = SCPI_CoreCls,}, \
+    { .pattern = "*ESE", .callback = SCPI_CoreEse,}, \
+    { .pattern = "*ESE?", .callback = SCPI_CoreEseQ,}, \
+    { .pattern = "*ESR?", .callback = SCPI_CoreEsrQ,}, \
+    { .pattern = "*IDN?", .callback = SCPI_CoreIdnQ,}, \
+    { .pattern = "*OPC", .callback = SCPI_CoreOpc,}, \
+    { .pattern = "*OPC?", .callback = SCPI_CoreOpcQ,}, \
+    { .pattern = "*RST", .callback = SCPI_CoreRst,}, \
+    { .pattern = "*SRE", .callback = SCPI_CoreSre,}, \
+    { .pattern = "*SRE?", .callback = SCPI_CoreSreQ,}, \
+    { .pattern = "*STB?", .callback = SCPI_CoreStbQ,}, \
+    { .pattern = "*TST?", .callback = My_CoreTstQ,}, \
+    { .pattern = "*WAI", .callback = SCPI_CoreWai,}, \
+ \
+    /* Required SCPI commands (SCPI std V1999.0 4.2.1) */ \
+    {.pattern = "SYSTem:ERRor[:NEXT]?", .callback = SCPI_SystemErrorNextQ,}, \
+    {.pattern = "SYSTem:ERRor:COUNt?", .callback = SCPI_SystemErrorCountQ,}, \
+    {.pattern = "SYSTem:VERSion?", .callback = SCPI_SystemVersionQ,}, \
+ \
+ \
+    {.pattern = "STATus:OPERation:EVENt?", .callback = SCPI_StatusOperationEventQ,}, \
+    {.pattern = "STATus:OPERation:CONDition?", .callback = SCPI_StatusOperationConditionQ,}, \
+    {.pattern = "STATus:OPERation:ENABle", .callback = SCPI_StatusOperationEnable,}, \
+    {.pattern = "STATus:OPERation:ENABle?", .callback = SCPI_StatusOperationEnableQ,}, \
+ \
+    {.pattern = "STATus:QUEStionable[:EVENt]?", .callback = SCPI_StatusQuestionableEventQ,}, \
+    {.pattern = "STATus:QUEStionable:CONDition?", .callback = SCPI_StatusQuestionableConditionQ,}, \
+    {.pattern = "STATus:QUEStionable:ENABle", .callback = SCPI_StatusQuestionableEnable,}, \
+    {.pattern = "STATus:QUEStionable:ENABle?", .callback = SCPI_StatusQuestionableEnableQ,}, \
+ \
+    {.pattern = "STATus:PRESet", .callback = SCPI_StatusPreset,},
+
+
+scpi_result_t My_CoreTstQ(scpi_t * context);
+
+
+#endif // SCPI_SCPI_BASE_H
---
+++ scpi/scpi_base.c
@@ -0,0 +1,17 @@
+#include "scpi/scpi_base.h"
+
+/**
+ * Reimplement IEEE488.2 *TST?
+ *
+ * Result should be 0 if everything is ok
+ * Result should be 1 if something goes wrong
+ *
+ * Return SCPI_RES_OK
+ */
+scpi_result_t My_CoreTstQ(scpi_t * context) {
+
+    SCPI_ResultInt32(context, 0);
+
+    return SCPI_RES_OK;
+}
+
