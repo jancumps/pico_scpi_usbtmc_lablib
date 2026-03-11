2f76d80ce9361a7bec5ff024b4864cd6657b84ab
Michael Stoops <3201502+michaelstoops@users.noreply.github.com>
2026-03-11 15:58:08 -0700

    Changed `desc_device.bcdUSB` to `USB_BCD`
    `USB_BCD=0x0200` Is the correct constant and value for
    `desc_device.bcdUSB`. See
    https://github.com/hathach/tinyusb/blob/f2450788b13f8424b2f75e33240ed21329a875ad/examples/device/usbtmc/src/usb_descriptors.c#L51
    
    This was a bug, introduced way back in
    https://github.com/jancumps/pico_scpi_usbtmc_labtool/commit/2f06b2657bcd28bc7335693fc50220ea9875839b#diff-ae622bf3c8308257869dc927958152cae5a47742748d243bfd1bef8f37b7e923L56-R59.

--- usb/usb_descriptors_common.c
+++ usb/usb_descriptors_common.c
@@ -64,7 +64,7 @@ tusb_desc_device_t const desc_device =
 {
     .bLength            = sizeof(tusb_desc_device_t),
     .bDescriptorType    = TUSB_DESC_DEVICE,
-    .bcdUSB             = 0x0200,
+    .bcdUSB             = USB_BCD,
     .bDeviceClass       = 0x00,
     .bDeviceSubClass    = 0x00,
     .bDeviceProtocol    = 0x00,
